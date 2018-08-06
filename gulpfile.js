722553b6bb06071f4e18b349408d37f706f7a954
Marcela Bomfim <mbomfim@live.com>
2018-08-05 23:06:53 -0300

    Add serve task and npm dev script

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
 // compile .scss to .css files
 gulp.task('sass', function() {
   return gulp.src(['src/scss/*.scss'])
     .pipe(sass())
-    .pipe(gulp.dest("dist/css"));
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
 });
+
+// watch sass & serve
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: "./"
+  });
+  gulp.watch(['src/scss/*.scss'], ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// default task
+gulp.task('default', ['serve']);
